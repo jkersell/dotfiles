9700f1a3b1848b606f83542f66f38cb230c3e22c
Jazz Kersell <jkersell@evertz.com>
2024-07-11 11:54:13 -0400

    neovim: Set up a Python provider for Python based plugins

--- neovim/init.vim
+++ neovim/init.vim
@@ -1,5 +1,8 @@
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
+
+let g:python3_host_prog = "$HOME/.pynvim/bin/python"
+
 source ~/.vimrc
 
 lua require("init")
--- neovim/install.sh
+++ neovim/install.sh
@@ -6,3 +6,8 @@ cp --verbose .vimrc "$HOME"/.vimrc
 cp --verbose init.vim "$nvim_config_dir"/init.vim
 cp --verbose scripts.vim "$nvim_config_dir"/scripts.vim
 cp --recursive --verbose --no-target-directory lua "$nvim_config_dir"/lua
+
+# Set up a virtual environment to install pynvim in
+pynvim_venv="$HOME"/.pynvim
+python3 -m venv "$pynvim_venv"
+"$pynvim_venv"/bin/python3 -m pip install --upgrade pynvim
