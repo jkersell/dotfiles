ea4c0870fbeb60a7da2fd1a28a3b52a9064d14d3
Jazz Kersell <jazzkersell@gmail.com>
2026-03-26 15:28:18 -0400

    neovim: Mofify install_packages.sh to support multiple distros
    Originally I used this script only an my work machine that was running
    Ubuntu. Now I use it on my personal machine which is running Manjaro. I
    don't want to lose the ability to use this with Ubuntu so this commit
    modifies the install script to pick the correct distro.

--- neovim/install_packages.sh
+++ neovim/install_packages.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# nvim can use tmux as a clipboard provider, so xsel
-# is only required if tmux is not available
-apt-get install \
-    xsel;
+# XXX: Originally I was managing configuration and packages on an Ubuntu machine
+# so some packages have been tested on Ubuntu, others not. It may be necessary
+# to create platform specific lists of packages in the future.
+PACKAGES=(
+    xsel # nvim can use tmux as a clipboard provider, so xsel is only required
+         #  if tmux is not available
+)
+
+source /etc/os-release
+
+case ${ID} in
+    ubuntu) apt-get install "${PACKAGES[@]}";;
+    manjaro) pamac install "${PACKAGES[@]}";;
+esac
