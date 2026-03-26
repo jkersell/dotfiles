618b7e3428aa032b79ff08fde57c57594a01cd8e
Jazz Kersell <jazzkersell@gmail.com>
2026-03-26 15:38:46 -0400

    neovim: Documment use of Treesitter

--- neovim/lua/init.lua
+++ neovim/lua/init.lua
@@ -13,6 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
+-- Treesitter used for syntax highlighting
 vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'go', 'python', 'lua' },
     callback = function()
